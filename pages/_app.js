9c86954045e4ac5450aef708b9e73839013e5233
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-04-04 17:10:54 +0900

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
-import Head from 'next/head';
+import Head from "next/head";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-    return (
-      <>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
-  
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+        />
+        <link rel="icon" href="/static/favicon16.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}

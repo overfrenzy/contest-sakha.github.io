20dae9cf1abdaf968b9df2992d8a3c6d174d2e98
overfrenzy <xplo1d387@gmail.com>
2022-06-12 20:05:13 +0900

    Prototype

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
-import Link from "next/link";
-
-import styles from "../styles/index.module.css";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import styles from '../styles/index.module.css';
+import Container from '@mui/material/Container';
+import AppBar from '../public/AppBar';
 
 export default function HomePage() {
 	return (
 		<div className={styles.content}>
-			<h1>Олимпиады по программированию Республики Саха (Якутия)</h1>
-			<p><Link href="/tuy-2005">Туймаада-2005</Link></p>
-			<p><Link href="/tuy-2016">Туймаада-2016</Link></p>
-			<p><Link href="/tuy-2021">Туймаада-2021</Link></p>
-		</div>
+			<AppBar />
+			<Container sx={{ marginY: 3 }}>
+				<h1>Олимпиады по программированию Республики Саха (Якутия)</h1>
+				<ButtonGroup variant="contained" color="primary">
+					<Button href="/tuy-2005">
+						Туймаада-2005
+					</Button>
+					<Button href="/tuy-2016">
+						Туймаада-2016
+					</Button>
+					<Button href="/tuy-2021">
+						Туймаада-2021
+					</Button>
+				</ButtonGroup>
+			</Container>
+		</div >
 	);
 }
--- pages/tuy-2021.tsx
+++ pages/tuy-2021.tsx
@@ -1,22 +1,68 @@
-import Head from 'next/head';
+import styles from "../styles/tuy-2016.module.css";
+import Container from '@mui/material/Container';
+import AppBar from "../public/AppBar";
 
 export default function Home(props) {
 	const students = props.students;
 	return (
-		<div style={{ padding: 30 }}>
-			<Head>
-				<title>2021</title>
-			</Head>
-			<div>
-				{students.map((post) =>
-					<div
-						key={students.id}
-						style={{ padding: 20, borderBottom: '1px solid #ccc' }}>
-						<h2>{post.participant_ru}</h2>
-						<p>{post.country}</p>
-					</div>)}
-			</div>
-		</div>
+		<>
+			<AppBar />
+			<Container>
+				<h1>Tuymaada-2021</h1>
+				<h2>Протокол</h2>
+				<table className={styles.table}>
+					<thead>
+						<tr>
+							<th rowSpan={2}>№</th>
+							<th rowSpan={2}>ФИО участника / Full name</th>
+							<th rowSpan={2}>Страна / Country</th>
+							<th colSpan={3}>Задания 1 дня / First day problems</th>
+							<th rowSpan={2}>Итого1 / Total1</th>
+							<th colSpan={3}>Задания 2 дня / Second day problems</th>
+							<th rowSpan={2}>Итого2 / Total2</th>
+							<th rowSpan={2}>Итого / Total</th>
+							<th rowSpan={2}>Место / Place</th>
+						</tr>
+						<tr>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+							<th>1</th>
+							<th>2</th>
+							<th>3</th>
+						</tr>
+					</thead>
+					<tbody>
+						{students.map((elem, i) => (
+							<tr>
+								<td>{i + 1}</td>
+								<td>
+									{elem.participant_ru} / {elem.participant_en}
+								</td>
+								<td>{elem.country}</td>
+								<td className={styles.value}>{elem.problems1[0]}</td>
+								<td className={styles.value}>{elem.problems1[1]}</td>
+								<td className={styles.value}>{elem.problems1[2]}</td>
+								<td className={styles.value}>
+									{elem.problems1.reduce((s, v) => (s += v), 0)}
+								</td>
+								<td className={styles.value}>{elem.problems2[0]}</td>
+								<td className={styles.value}>{elem.problems2[1]}</td>
+								<td className={styles.value}>{elem.problems2[2]}</td>
+								<td className={styles.value}>
+									{elem.problems2.reduce((s, v) => (s += v), 0)}
+								</td>
+								<td className={styles.value}>
+									{elem.problems1.reduce((s, v) => (s += v), 0) +
+										elem.problems2.reduce((s, v) => (s += v), 0)}
+								</td>
+								<td className={styles.value}>{elem.place}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</Container>
+		</>
 	);
 }
 
@@ -24,7 +70,7 @@ export default function Home(props) {
 import fsPromises from 'fs/promises';
 import path from 'path';
 export async function getStaticProps() {
-	const filePath = path.join(process.cwd(), './data2021.json');
+	const filePath = path.join(process.cwd(), './public/data2021.json');
 	const jsonData = await fsPromises.readFile(filePath);
 	const objectData = JSON.parse(jsonData.toString());
 
